26b3672a905ffab140653f3f6a94f6be875f57d8
P1XY <p1xy@local.dev>
2026-07-15 13:26:12 +0300

    temp save2

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,2 +1,20 @@
 from dotenv import load_dotenv
+
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+ENV_FILE = BASE_DIR / ".env"
+
+load_dotenv(ENV_FILE)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError(
+        "BOT_TOKEN is not configured."
+        " Please check your .env file."
+    )
+
+
+
+
