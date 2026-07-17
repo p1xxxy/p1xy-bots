ec220da613d1a3d564cb672115f22744d47592a6
p1xy <den.parnishka@gmail.com>
2026-07-17 22:44:13 +0300

    chore: 1st handler created

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 import asyncio
 from aiogram import Bot
+from aiogram import Dispatcher
+from app.config import settings
+
+dp = Dispatcher()
 
 async def main():
     print("Application started")
-    
+    bot = Bot(token=settings.BOT_TOKEN)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
     
-BOT_TOKEN = Bot()
+
