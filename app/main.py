9b57a0c72c80d365d6c4c13be58a1e9d5365edc5
P1XY <p1xy@local.dev>
2026-07-17 15:16:06 +0300

    temp save 5

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-import os
+import asyncio
+from aiogram import Bot
 
 async def main():
-    print("Hellop")
+    print("Application started")
     
-    
-    
-if __name__ == "__main__":
-        print("B")
 
+if __name__ == "__main__":
+    asyncio.run(main())
+    
+BOT_TOKEN = Bot()
