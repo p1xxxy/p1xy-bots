7b6e2f3ffd4eeec59b11a60751c57d86b6360169
p1xy <den.parnishka@gmail.com>
2026-07-23 17:47:45 +0300

    temp savehz

--- app/main.py
+++ app/main.py
@@ -1,9 +1,27 @@
 import asyncio
 from aiogram import Bot
 from aiogram import Dispatcher
+from aiogram import Router
+from aiogram.filters import CommandStart
 from app.config import settings
+from aiogram.fsm.state import StatesGroup
+from aiogram.fsm.state import State
+from aiogram.fsm.storage.memory import MemoryStorage
 
-dp = Dispatcher()
+
+dp = Dispatcher(storage=MemoryStorage())
+router = Router()
+dp.include_router(router)
+
+class AddClient(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_email = State()
+    waiting_for_phone = State()
+
+@router.message(CommandStart())
+async def command_start_handler(message):
+    await message.answer("Hello, world!")
+    
 
 async def main():
     print("Application started")
