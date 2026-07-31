a332d2023f615e58e5b6f6382dacfbb60beb081b
p1xy <den.parnishka@gmail.com>
2026-07-31 22:24:42 +0300

    add roles

--- app/config/db.py
+++ app/config/db.py
@@ -13,9 +13,22 @@ async def init_db():
                 name TEXT NOT NULL,
                 phone TEXT NOT NULL,
                 email TEXT,
+                
                 created_at TEXT NOT NULL
             )
         """)
+        await conn.commit()
+
+async def init_roles():
+    """Добавляет роли в базу данных, если их ещё нет."""
+    async with aiosqlite.connect(DB_PATH) as conn:
+        await conn.execute("""
+            CREATE TABLE IF NOT EXISTS roles (
+                user_id INTEGER PRIMARY KEY,
+                role_name TEXT NOT NULL,
+                created_at TEXT NOT NULL,
+                updated_at TEXT
+            )""")
         await conn.commit()
         
 async def add_client(name: str, phone: str, email:str | None) -> None:
@@ -25,7 +38,17 @@ async def add_client(name: str, phone: str, email:str | None) -> None:
             (name, phone, email, datetime.now().isoformat())
         )
         await conn.commit()
-        
+
+async def add_role(user_id: int, role_name: str) -> None:
+    async with aiosqlite.connect(DB_PATH) as conn:
+        now = datetime.now().isoformat()
+        await conn.execute(
+            "INSERT INTO roles (user_id, role_name, created_at, updated_at) VALUES (?, ?, ?, NULL)"
+            " ON CONFLICT(user_id) DO UPDATE SET role_name=excluded.role_name, updated_at=?",
+            (user_id, role_name, now, now)
+        )
+        await conn.commit()
+
 async def get_all_clients() -> list:
     async with aiosqlite.connect(DB_PATH) as conn:
          cursor = await conn.execute("SELECT * FROM clients")
